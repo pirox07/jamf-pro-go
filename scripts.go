5ed969b9961712440e01be370840b4dd67c22b45
pirox07 <pirox.07@gmail.com>
2021-06-13 12:33:59 +0900

    feat(scripts): added CreateScript function

--- scripts.go
+++ scripts.go
@@ -84,3 +84,52 @@ func (c *Client) GetScript(scriptID uint32) (*Script, error) {
 
 	return &result, nil
 }
+
+type CreateScriptResult struct {
+	ID	string `json:"id"`
+	Href string `json:"href"`
+}
+
+type CreateScriptParams struct {
+	// Script ID
+	ID             string `json:"id,omitempty"`
+	// Display name
+	Name           string `json:"name"`
+	// Information to be displayed to the administrator when the script is run
+	Info           string `json:"info,omitempty"`
+	// Notes to be displayed about the script (e.g. author, creation date)
+	Notes          string `json:"notes,omitempty"`
+	// Priority to be used for executing scripts related to other actions
+	// [ BEFORE, AFTER, AT_REBOOT ] (default: BEFORE)
+	Priority       string `json:"priority,omitempty"`
+	// Category to which the script will be added
+	CategoryID     string `json:"categoryId"`
+	CategoryName   string `json:"categoryName,omitempty"`
+	// Parameter label Names
+	Parameter4     string `json:"parameter4,omitempty"`
+	Parameter5     string `json:"parameter5,omitempty"`
+	Parameter6     string `json:"parameter6,omitempty"`
+	Parameter7     string `json:"parameter7,omitempty"`
+	Parameter8     string `json:"parameter8,omitempty"`
+	Parameter9     string `json:"parameter9,omitempty"`
+	Parameter10    string `json:"parameter10,omitempty"`
+	Parameter11    string `json:"parameter11,omitempty"`
+	// Operating system requirements (e.g., "10.6.8, 10.7.x, 10.8")
+	OsRequirements string `json:"osRequirements"`
+	// Script code
+	ScriptContents string `json:"scriptContents"`
+}
+
+func (c *Client) CreateScript (params CreateScriptParams) (*CreateScriptResult, error) {
+	var result CreateScriptResult
+
+	fmt.Println(params)
+
+	err := c.call(path.Join(APIVersionScripts, APIPathScripts), http.MethodPost,
+		APIVersionScripts, nil, params, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
